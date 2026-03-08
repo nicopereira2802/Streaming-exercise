d3626fea70f1d7fefb4d6ce13af5cdccbcecc997
Nicolas Pereira <nicopereira2802@gmail.com>
2026-03-08 13:53:41 -0300

    correcciones parse

--- scr/models.py
+++ scr/models.py
@@ -1,22 +1,18 @@
 class Episode:
-    def __init__(self,series_name,season,episode_number,title,date,line):
+    def __init__(self, series_name, season, episode_number, title, date, line):
         self.series = str(series_name).strip()
         self.season = season
-        self.number = self.number
-        self.title = str(title).strip or "Untitle"
-        self.date = str(date).strip
+        self.number = episode_number
+        self.title = str(title).strip() or "Untitled Ep"
+        self.date = str(date).strip()
 
         self.line = line
-        self.is_valid = True  # es verdadero hasta que la validacion diga lo contrario
-        self.errors = []     #se guarda el motivo del fallo de validar
-
+        self.is_valid = True
+        self.errors = []
 
     def __repr__(self):
         return f"Episode(Line {self.line}: {self.series} S{self.season} E{self.number})"
-    
-    
+
     def get_uid(self):
-        #clave unica para depuplicacion
-        # usa el nombre, temporada y numero
-        return (self.series.lower(), str(self.season), str(self.number))
-    
+        # clave unica para deduplicacion
+        return (self.series.lower(), self.season, self.number)
--- scr/parser.py
+++ scr/parser.py
@@ -2,10 +2,11 @@ import csv
 from models import Episode
 
 def load_episodes(file_path):
+
     lista_resultado = []
 
-    with open(file_path,mode='r',encoding='utf-8') as f:
-        #dict reader lee como diccionario columna:valor
+    with open(file_path, mode='r', encoding='utf-8') as f:
+            #el dict reader usa como diccionario columna:valor
         reader = csv.DictReader(f)
 
         for i, fila in enumerate(reader, start=2):
@@ -14,11 +15,12 @@ def load_episodes(file_path):
 
                 series_name=fila['series_name'],
                 season=fila['season_number'],
-                number=fila['episode_number'],
+                episode_number=fila['episode_number'],
                 title=fila['episode_title'],
                 date=fila['air_date'],
                 line=i
             )
+
             lista_resultado.append(last_ep)
 
     return lista_resultado
